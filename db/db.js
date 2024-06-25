26f05685b418550f85accca0af22950a0d1d9a6d
solyats <solsstary@gmail.com>
2024-06-25 20:41:50 +0700

    FEAT

--- db/db.js
+++ db/db.js
@@ -1,14 +1,17 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const db = require('knex')({
-    client: 'mysql',
-    connection: {
-      host: process.env.DB_URL,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+const db = require("knex")({
+  client: "mysql",
+  connection: {
+    host: process.env.DB_URL,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+      rejectUnauthorized: process.env.DB_SSL,
     },
-  });
+  },
+});
 
 module.exports = db;
